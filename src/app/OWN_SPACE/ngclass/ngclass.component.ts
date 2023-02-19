09a8736ed2114400ea49e2ab3f715856cf205b48
YOGESH606 <76579825+YOGESH606@users.noreply.github.com>
2023-02-19 23:06:06 +0530

    change:  index.html file

--- src/app/OWN_SPACE/ngclass/ngclass.component.ts
+++ src/app/OWN_SPACE/ngclass/ngclass.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector:'ngclass',
-  templateUrl:'/ngclass.component.html',
+  templateUrl:'./ngclass.component.html',
   styleUrls:['./ngclass.component.css']
 })
 
